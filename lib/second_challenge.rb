730a86b53573c45a3cb597212e106fc00f12bafa
Carl Michael Oyam <carloyam21@icloud.com>
2020-09-19 11:09:43 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,5 +1,10 @@
 def second_challenge
+<<<<<<< HEAD
 epic_tragedy = {
+=======
+  epic_tragedy = {
+    epic_tragedy = {
+>>>>>>> 57a4efab45b004a6ef72f8ee1890dcadcf4eb4a7
    :montague => {
       :patriarch => {},
       :matriarch => {},
@@ -13,4 +18,9 @@ epic_tragedy = {
       :heroine_friends => []
    }
 }
+<<<<<<< HEAD
+=======
+
+  
+>>>>>>> 57a4efab45b004a6ef72f8ee1890dcadcf4eb4a7
 end
--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -4,13 +4,27 @@ epic_tragedy = {
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
+<<<<<<< HEAD
       :hero_friends => []
+=======
+      :hero_friends => [
+         {name: "Benvolio", age: "17", attitude: "worried"},
+         {name: "Mercutio", age: "18", attitude: "hot-headed"}
+      ]
+>>>>>>> 57a4efab45b004a6ef72f8ee1890dcadcf4eb4a7
    },
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
+<<<<<<< HEAD
       :heroine_friends => []
+=======
+      :heroine_friends => [
+          {name: "Steven", age: "30", attitude: "confused"},
+          {name: "Nurse", age: "44", attitude: "worried"}
+      ]
+>>>>>>> 57a4efab45b004a6ef72f8ee1890dcadcf4eb4a7
    }
 }
  
